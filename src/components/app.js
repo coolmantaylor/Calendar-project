e38d988ae1e191215571c93068d42be893e83027
coolmantaylor <tayesmy@gmail.com>
2019-11-12 14:59:30 -0700

    final commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,20 +3,43 @@ import React, { Component } from 'react';
 import Weekday from "./weekday"
 import DayBlock from "./dayBlock"
 
+import monthData from "../../static/assets/dummyData.json"
+
 export default class App extends Component {
   constructor(props) {
     super(props)
 
     this.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"]
+    this.months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    this.month = "November"
+    
+    this.state = monthData[this.month]
 
-    this.state = {
-      month: "November",
-      year: "2019",
-      daysInMonth: 30,
-      daysInPreviousMonth: 31,
-      startDay: "Friday"
-    }
+    this.handleMonthChange = this.handleMonthChange.bind(this)
   }
+
+  handleMonthChange(direction) {
+    let currentMonthIndex = this.months.indexOf(this.month)
+
+    if(direction === "+") {
+      currentMonthIndex++
+    }else {
+      currentMonthIndex--
+    }
+
+    if (currentMonthIndex === 12) {
+      currentMonthIndex = 0
+    }
+
+    if (currentMonthIndex === -1) {
+      currentMonthIndex = 11
+    }
+
+    this.month = this.months[currentMonthIndex]
+
+    this.setState(monthData[this.month])
+  }
+
   renderDays = () => {
     
     return this.days.map(day => <Weekday day= {day} />)
@@ -28,7 +51,7 @@ export default class App extends Component {
     let fillerDate = this.state.daysInPreviousMonth - this.days.indexOf(this.state.startDay) + 1
 
     while (fillerDate <= this.state.daysInPreviousMonth) {
-      blocks.push(<DayBlock date={fillerDate} />)
+      blocks.push(<DayBlock date={fillerDate} filler={true}/>)
       fillerDate++
     }
 
@@ -41,27 +64,29 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div className="header">
-          <button>Previous Month</button>
-          <h1>{this.state.month}</h1>
-          <button>Next Month</button>
-        </div>
-
-
-        <div className="calender-wrapper">
-          <div className="days_wrapper">
-           {this.renderDays()}
+        <div className="content-wrapper">
+          <div className="header">
+            <button onClick = {() => this.handleMonthChange("-")}>Previous Month</button>
+            <h1>{this.state.month}</h1>
+            <button onClick = {() => this.handleMonthChange("+")}>Next Month</button>
           </div>
 
 
-          <div className="blocks-wrapper">
-            {this.renderBlocks()}
+          <div className="calendar-wrapper">
+            <div className="days-wrapper">
+              {this.renderDays()}
+            </div>
+
+
+            <div className="blocks-wrapper">
+              {this.renderBlocks()}
+            </div>
           </div>
-        </div>
 
 
-        <div className="footer">
-          <h1>{this.state.year}</h1>
+          <div className="footer">
+            <h1>{this.state.year}</h1>
+          </div>
         </div>
       </div>
     );
